2e2f93d6eb7b9895f994d5ff17396a4016923b0f
hogepiyosan <temporary.nk@gmail.com>
2016-02-12 22:22:30 +0900

    Sanitize the message
    Speech printable ascii only

--- mods/Text to Speech/lua/chatmanager.lua
+++ mods/Text to Speech/lua/chatmanager.lua
@@ -1,10 +1,18 @@
 local mod_path = mod_path or ModPath
-local _STSpeech = assert(package.loadlib(mod_path .. "bin/pd2tts.dll", "luaSTSpeech"))
+_G._STSpeech = _G._STSpeech or assert(package.loadlib(mod_path .. "bin/pd2tts.dll", "luaSTSpeech"))
 
 function STSpeech(message)
+	local function Sanitize(msg)
+		local r = ""
+		for s in msg:gmatch("[%w%p]+") do
+			r = r .. s .. " "
+		end
+		return r
+	end
+	
 	local f = io.open( mod_path .. "bin/msg.dat", "w" )
 	if f then
-		f:write(message)
+		f:write(Sanitize(message))
 		f:close()
 		_STSpeech()
 	end
