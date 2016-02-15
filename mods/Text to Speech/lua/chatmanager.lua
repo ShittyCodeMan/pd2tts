1efffcc66c7292c482ebec5e76d7ebff9df361ab
hogepiyosan <temporary.nk@gmail.com>
2016-02-15 22:44:06 +0900

    Remove Sanitize function
    for japanese mod (temp-fix and never fixed probably :expressionless: )

--- mods/Text to Speech/lua/chatmanager.lua
+++ mods/Text to Speech/lua/chatmanager.lua
@@ -2,17 +2,9 @@ local mod_path = mod_path or ModPath
 _G._STSpeech = _G._STSpeech or assert(package.loadlib(mod_path .. "bin/pd2tts.dll", "luaSTSpeech"))
 
 function STSpeech(message)
-	local function Sanitize(msg)
-		local r = ""
-		for s in msg:gmatch("[%w%p]+") do
-			r = r .. s .. " "
-		end
-		return r
-	end
-	
 	local f = io.open( mod_path .. "bin/msg.dat", "w" )
 	if f then
-		f:write(Sanitize(message))
+		f:write(message)
 		f:close()
 		_STSpeech()
 	end
